84bfad8ee94c94267788a7bf45a82055aba4e947
ungericw <ungericw@tutanota.com>
2022-05-26 10:38:19 -0500

    Add project files.

---
+++ EricUng3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EricUng3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
